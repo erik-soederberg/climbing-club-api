1d179f4306885ad9bbdec294a84405dd9365649a
erik-soederberg <eriksoederberg@outlook.com>
2026-09-11 21:47:30 +0200

    feat: implement GET /routes

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-
-
-
-
-
+app.get('/routes', (req, res) => {
+    const filePath = path.join(__dirname, 'data', 'routes.json');
+    const fileContent = fs.readFileSync(filePath, 'utf8');
+    const routes = JSON.parse(fileContent);
+    res.json(routes);
+  });
 
 module.exports = app;
